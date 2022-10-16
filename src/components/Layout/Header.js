57c60f598f7caa9d4e9467adfc8fd702a7be3b79
Brainybrian316 <brainybrian316@gmail.com>
2022-10-16 17:59:34 -0400

    header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+	return (
+		// shorter syntax for react fragment
+		<>
+			<header className={classes.header}>
+				<h1>ReactMeals</h1>
+				<button>Cart</button>
+			</header>
+			{/* we cannot use . notation since there is a - for the two words so we have to write out like this */}
+			<div className={classes['main-image']}>
+				<img src={mealsImage} alt="A table full of delicious food!" />
+			</div>
+		</>
+	);
+};
+
+export default Header;
