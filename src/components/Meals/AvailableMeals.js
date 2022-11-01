adfeb676982c0721425881ee9e895123f0b15f6b
Brian Mojica <bmojica@lukeassoc.com>
2022-11-01 16:55:59 -0400

    meal form and reusuable input

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,17 @@
 import classes from './AvailableMeals.module.css';
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import DUMMY_MEALS from './MealData.json';
 
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+	const mealsList = DUMMY_MEALS.map((meal) => (
+		<MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+	));
 	return (
 		<section className={classes.meals}>
-			<ul>{mealsList}</ul>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
 		</section>
 	);
 };
